926f3855e6f8d1d3b8c5c149d166577f0f046ed6
aperezsimon <aperezsimon90@gmail.com>
2021-04-18 15:22:09 +0200

    Test core logger classes0

--- app/src/main/java/com/aperezsi/diablobuddy/MainActivity.kt
+++ app/src/main/java/com/aperezsi/diablobuddy/MainActivity.kt
@@ -1,24 +1,24 @@
 package com.aperezsi.diablobuddy
 
 import android.os.Bundle
-import android.util.Log
-import androidx.core.os.bundleOf
 import com.aperezsi.core.BaseActivity
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import com.aperezsi.core.logger.AnalyticsLogger
+import com.aperezsi.core.logger.CrashlyticsLogger
+import com.aperezsi.core.logger.Logger
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: BaseActivity() {
+class MainActivity : BaseActivity() {
 
-    private val firebaseAnalytics = Firebase.analytics
+    private val crashLogger = CrashlyticsLogger(FirebaseCrashlytics.getInstance())
+    private val trackLogger = AnalyticsLogger()
+    private val logger = Logger(crashLogger, trackLogger)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         productFlavour.text = BuildConfig.APPLICATION_ID
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundleOf(
-            FirebaseAnalytics.Param.ITEM_ID to BuildConfig.APPLICATION_ID
-        ))
+        logger.logEvent()
+        logger.logCrashInfo("Esto es un crash")
     }
 }
