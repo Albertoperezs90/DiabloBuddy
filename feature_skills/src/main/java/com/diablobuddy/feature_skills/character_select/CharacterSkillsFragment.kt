b0b09bc7473678b08a0d8bb3633cc30016c43cf3
aperezsimon <aperezsimon90@gmail.com>
2022-02-10 00:41:40 +0100

    First approach to motion layout

--- feature_skills/src/main/java/com/diablobuddy/feature_skills/character_select/CharacterSkillsFragment.kt
+++ feature_skills/src/main/java/com/diablobuddy/feature_skills/character_select/CharacterSkillsFragment.kt
@@ -1,7 +1,9 @@
 package com.diablobuddy.feature_skills.character_select
 
+import androidx.appcompat.widget.SearchView
 import com.diablobuddy.core.framework.base.BaseFragment
 import com.diablobuddy.core.framework.provideViewModel
+import com.diablobuddy.core.utilities.clickhandler.setOnDebounceClickListener
 import com.diablobuddy.feature_skills.character_select.state.CharacterSkillsViewState
 import com.diablobuddy.feature_skills.databinding.FragmentCharacterskillsBinding
 import com.diablobuddy.feature_skills.inject
@@ -10,6 +12,8 @@ class CharacterSkillsFragment: BaseFragment<FragmentCharacterskillsBinding, Char
 
     override val viewModel: CharacterSkillsViewModel by lazy { provideViewModel(CharacterSkillsViewModel::class) }
 
+    private var sceneState = 0
+
     override fun inflate(): FragmentCharacterskillsBinding = FragmentCharacterskillsBinding.inflate(layoutInflater)
 
     override fun injectDependencies() {
@@ -17,7 +21,21 @@ class CharacterSkillsFragment: BaseFragment<FragmentCharacterskillsBinding, Char
     }
 
     override fun initialize() {
-        // Do nothing
+        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // do nothing
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.orEmpty().isNotEmpty()) {
+                    binding.motionScene.transitionToEnd()
+                } else {
+                    binding.motionScene.transitionToStart()
+                }
+                return true
+            }
+        })
     }
 
     override fun render(viewState: CharacterSkillsViewState) {
